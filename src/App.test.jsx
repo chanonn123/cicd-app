f7d52f1d77d392695d25db9fd46b9620b4225511
Chan Onn <chanonn0103@gmail.com>
2024-10-10 12:05:00 +0800

    hotfix: fix email address format

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -9,6 +9,6 @@ test("App form submission", async () => {
     const emailInput = screen.getByTestId("emailInput");
     const submitButton = screen.getByTestId("submitButton");
 
-    await userEvent.type(emailInput, 'sigmaschool');
+    await userEvent.type(emailInput, 'sigmaschool@gmail.com');
     expect(submitButton.disabled).toBe(false);
 })
